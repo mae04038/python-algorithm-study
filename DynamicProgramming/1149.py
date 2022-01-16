d66e19c76cc14b2c02bf8b2a823a4578950c33bc
HEEJEONG <mae04038@naver.com>
2022-01-16 16:09:19 +0900

    DP

--- DynamicProgramming/1149.py
+++ DynamicProgramming/1149.py
@@ -1,9 +1,15 @@
 n = int(input())  # 집의 개수
-cost = []
+RGB = []
 for _ in range(n):
-    cost.append(list(map(int, input().split())))
-result = 0  # 최종 비용
+    RGB.append(list(map(int, input().split())))
 
-dp = [0] * 1001
-dp[1] = min(cost[0])
-print(dp[1])
+# 빨간집을 골랐을 때, 초록집을 골랐을 때, 파란집을 골랐을 때 각각의 경우 따져서
+# 최종적으로 비용이 적게드는 것 고르기
+for i in range(1, n):
+    # 빨간집
+    RGB[i][0] = min(RGB[i-1][1], RGB[i-1][2]) + RGB[i][0]
+    # 초록집
+    RGB[i][1] = min(RGB[i-1][0], RGB[i-1][2]) + RGB[i][1]
+    # 파란집
+    RGB[i][2] = min(RGB[i-1][0], RGB[i-1][1]) + RGB[i][2]
+print(min(RGB[n-1]))
