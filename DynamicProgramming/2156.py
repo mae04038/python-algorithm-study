04c2340299cc2840e89510914126960b31a81acc
HEEJEONG <mae04038@naver.com>
2022-01-25 23:23:08 +0900

    DP

--- DynamicProgramming/2156.py
+++ DynamicProgramming/2156.py
@@ -1,5 +1,12 @@
 n = int(input())  # 포도잔의 개수
-wine = []
+wine = [0]
 for _ in range(n):
     wine.append(int(input()))
-print(wine)
+
+dp = [0]
+dp.append(wine[1])
+if n > 1:
+    dp.append(wine[1]+wine[2])
+for i in range(3, n+1):
+    dp.append(max(dp[i-1], dp[i-2]+wine[i], dp[i-3]+dp[i-1]+wine[i]))
+print(dp[n])
