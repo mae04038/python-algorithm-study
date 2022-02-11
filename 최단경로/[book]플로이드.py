5187c5049a2941b2779b0d6f6a014512a641ca93
HEEJEONG <mae04038@naver.com>
2022-02-11 19:35:21 +0900

    플로이드워셜알고리즘

---
+++ 최단경로/[book]플로이드.py
@@ -0,0 +1,32 @@
+INF = int(1e9)
+
+# 도시개수랑 버스노선입력받기
+city = int(input())
+bus = int(input())
+
+graph = [[INF]*(city+1) for _ in range(city+1)]  # 초기화
+
+for a in range(1, city+1):  # 자기자신으로 가는 비용은 0
+    for b in range(1, city+1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 노선 정보 입력받고 초기화
+for _ in range(bus):
+    a, b, cost = map(int, input().split())
+    if cost < graph[a][b]:  # 가장 적은 비용을 저장
+        graph[a][b] = cost
+
+# 플로이드워셜알고리즘 수행
+for k in range(1, city+1):
+    for a in range(1, city+1):
+        for b in range(1, city+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+[k][b])
+
+for a in range(1, city+1):
+    for b in range(1, city+1):
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
